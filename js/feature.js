bd6d97d38c970738dc1bf12a579d3b85a1820696
Pedro Thales Sousa Pereira <pedrothalessp@gmail.com>
2024-12-09 19:33:24 -0300

    init Js

---
+++ js/feature.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("form");
+const firstName = document.getElementById("first-name");
+const secondName = document.getElementById ("secont-name");
+const userEmail = document.getElementById ("user-email");
+const queryType = document.getElementsByName ("user-enquiry");
+const mensage = document.getElementById("user-mensage");
+const checkBox = document.getElementById("user-checkbox");
+const userSubmit = document.getElementById("user-submit");
+
+form.addEventListener("submit", (event) => {
+
+    
+
+
+})
