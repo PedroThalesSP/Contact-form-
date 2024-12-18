b163bead63c782d309b448fb4bf2d6bf0117bb38
Pedro Thales Sousa Pereira <pedrothalessp@gmail.com>
2024-12-18 09:58:46 -0300

    js 3

--- js/feature.js
+++ js/feature.js
@@ -1,33 +1,57 @@
 const form = document.getElementById("form");
 const firstName = document.getElementById("first-name");
-const secondName = document.getElementById ("secont-name");
+const secondName = document.getElementById ("second-name");
 const userEmail = document.getElementById ("user-email");
-const queryType = document.getElementsByName ("user-enquiry");
 const mensage = document.getElementById("user-mensage");
 const checkBox = document.getElementById("user-checkbox");
 const userSubmit = document.getElementById("user-submit");
 
 form.addEventListener("submit", (event) => {
+    //tratamento para  que a página não carregue
     event.preventDefault();
 
 
-
+    // funções chamadas
     userNameValidation();
+    userNameValidationTwo();
+    userEmailValidation();
+    queryTypeOption();
 })
 
+//User name
 function userNameValidation(){
     const firstNameValidation = firstName.value;
     console.log(firstNameValidation)
 
     if (firstNameValidation === ""){
-        errorInput(firstName, "Digite seu primeiro nome" )
+       alert("Digite seu primeiro nome")
     }
 
 }
 
-function errorInput(input, message){
-    const formItem = input.parentElement;
-    const textMessage = form.querySelector("a")
-    textMessage.innerText = message;
-    formItem.className = "form-content error"
+//Second name
+function userNameValidationTwo(){
+    const secondNameValidation = secondName.value;
+    console.log(secondNameValidation)
+
+    if (secondNameValidation === ""){
+        alert("Digite seu segundo nome")
+    }
+}
+
+// Email
+function userEmailValidation(){
+    const emailValue = userEmail.value;
+    console.log(emailValue);
+}
+
+// Options
+function queryTypeOption() {
+    const selectedOption = document.querySelector('input[name="radio"]:checked'); // Seleciona o rádio marcado
+
+    if (selectedOption) {
+        console.log(`Opção selecionada: ${selectedOption.value}`);
+    } else {
+        alert("Por favor, selecione um tipo de consulta.");
+    }
 }
