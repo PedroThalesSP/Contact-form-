cba4d643b148c4a0ea512288a603bf37ccaf30c4
Pedro Thales Sousa Pereira <pedrothalessp@gmail.com>
2025-01-08 22:13:45 -0300

    terminando a validação

--- js/feature.js
+++ js/feature.js
@@ -12,12 +12,25 @@ form.addEventListener("submit", (event) => {
 
 
     // funções chamadas
-    userNameValidation();
-    userNameValidationTwo();
-    userEmailValidation();
-    queryTypeOption();
-    mensageValidation()
-    checkBoxValidation();
+    const isValidFirstName = userNameValidation();
+    const isValidSecondName = userNameValidationTwo();
+    const isValidEmail = userEmailValidation();
+    const isValidQueryOption = queryTypeOption();
+    const isValidMessage = mensageValidation()
+    const isValidCheckBox = checkBoxValidation();
+
+    console.log({
+        isValidFirstName,
+        isValidSecondName,
+        isValidEmail,
+        isValidQueryOption,
+        isValidMessage,
+        isValidCheckBox
+    });
+
+    if (isValidFirstName && isValidSecondName && isValidEmail && isValidQueryOption && isValidMessage && isValidCheckBox){
+        window.location.href = "success.html";
+    }
     
 })
 
@@ -28,8 +41,9 @@ function userNameValidation(){
 
     if (firstNameValidation === ""){
        alert("Digite seu primeiro nome")
+        return false;
     }
-
+    return true;
 }
 
 //Second name
@@ -39,13 +53,21 @@ function userNameValidationTwo(){
 
     if (secondNameValidation === ""){
         alert("Digite seu segundo nome")
+        return false;
     }
+    return true;
 }
 
 // Email
 function userEmailValidation(){
-    const emailValue = userEmail.value;
-    console.log(emailValue);
+    const emailValue = userEmail.value.trim();
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(!emailValue.match(emailPattern)){
+        alert("Por Favor, digite um email valido")
+        return false;
+    }
+    return true;
 }
 
 // Options
@@ -54,8 +76,11 @@ function queryTypeOption() {
 
     if (selectedOption) {
         console.log(`Opção selecionada: ${selectedOption.value}`);
+        return true;
+        
     } else {
         alert("Por favor, selecione um tipo de consulta.");
+        return false;
     }
 }
 
@@ -64,8 +89,11 @@ function mensageValidation (){
 
     if (userMensage === "" ){
         alert("digite sua mensagem")
+        return false;
     } else {
         console.log(userMensage)
+        return true;
+        
     }
 }
 
@@ -75,7 +103,9 @@ function checkBoxValidation (){
 
     if(!boxValidation.checked){
         alert("Marque a caixa abaixo para prosseguir")
+        return false;
     } else{ 
         console.log(`A opção foi selecionada com exito`)
+        return true;
     }
 }
